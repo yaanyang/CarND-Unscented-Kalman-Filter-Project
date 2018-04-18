c6ac42baa26c972f2aedd03f2dae8a0bd2b8e4a0
Chris Yang <Chris.Yang@wdc.com>
2018-04-18 15:18:26 -0700

    updated process noices

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -26,10 +26,10 @@ UKF::UKF()
     P_ = MatrixXd(5, 5);
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
-    std_a_ = 0.2;
+    std_a_ = 3;
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = 0.2;
+    std_yawdd_ = 0.3;
 
     //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
     // Laser measurement noise standard deviation position1 in m
